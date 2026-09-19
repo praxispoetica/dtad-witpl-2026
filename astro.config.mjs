ff95d28a3fd83d740564cd4b253f5a5d5da7bee7
victorkane <victorkane@gmail.com>
2026-09-18 22:11:14 -0300

    style: customization with navbar, including menu, social icons, and navigaton menu, etc.

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,23 +9,33 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: 'My Docs',
+			favicon: 'favicon.ico',
+			head: [
+    			// Add ICO favicon fallback for Safari.
+    			{
+			        tag: 'link',
+  				    attrs: {
+        				rel: 'icon',
+        				href: 'favicon.ico',
+        				sizes: '32x32',
+    		  			type: 'image/x-icon',
+					},
+    			},
+  			],
 			customCss: ['./src/styles/custom.css'],
 			components: {
 				SocialIcons: './src/components/HeaderLinks.astro'
 			},
 			plugins: [starlightThemeExquisitus()],
+			logo: {
+        		src: './src/assets/vk-1024x1024.jpg',
+      		},
 			social: [
-				{
-					icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight'
-
-				},
-				/* non-social social icon links can be added here, for example:
-				{
-					icon: 'document', label: 'About', href: '/about/'
-				},
-				{
-					icon: 'pencil', label: 'Notes', href: '/notes/'
-				}
+				{icon: 'github', label: 'GitHub', href: 'https://github.com/praxispoetica/dtad-witpl-2026'},
+				/* no space, so linktree style dot com will have to be enough for now
+				{icon: 'blueSky', label: 'Bluesky', href: 'https://bsky.app/profile/vokapelman.bsky.social'},
+				{icon: 'substack', label: 'Substack', href: 'https://vokapelman.substack.com'},
+				{icon: 'linkedin', label: 'LinkedIn', href: 'https://www.linkedin.com/in/victorkane' },
 				*/
 			],
 			sidebar: [
