5697e140f79a2fe73bf2b939e761b81c1389937a
victorkane <victorkane@gmail.com>
2026-09-20 09:01:03 -0300

    build: fix NavBar Menu items, add alt and title entries for hover info, add support for lucide icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,54 +3,56 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightThemeExquisitus from 'starlight-theme-exquisitus';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://vokapelman.online',
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			favicon: 'favicon.ico',
-			head: [
-    			// Add ICO favicon fallback for Safari.
-    			{
-			        tag: 'link',
-  				    attrs: {
-        				rel: 'icon',
-        				href: 'favicon.ico',
-        				sizes: '32x32',
-    		  			type: 'image/x-icon',
-					},
-    			},
+    integrations: [starlight({
+        title: 'My Docs',
+        favicon: 'favicon.ico',
+        head: [
+            // Add ICO favicon fallback for Safari.
+            {
+                tag: 'link',
+                attrs: {
+                    rel: 'icon',
+                    href: 'favicon.ico',
+                    sizes: '32x32',
+                    type: 'image/x-icon',
+                },
+            },
   			],
-			customCss: ['./src/styles/custom.css'],
-			components: {
-				SocialIcons: './src/components/HeaderLinks.astro'
-			},
-			plugins: [starlightThemeExquisitus()],
-			logo: {
+        customCss: ['./src/styles/custom.css'],
+        /*
+        */
+        components: {
+            SocialIcons: './src/components/HeaderLinks.astro'
+        },
+        plugins: [starlightThemeExquisitus()],
+        logo: {
         		src: './src/assets/vk-1024x1024.jpg',
-      		},
-			social: [
-				{icon: 'github', label: 'GitHub', href: 'https://github.com/praxispoetica/dtad-witpl-2026'},
-				/* no space, so linktree style dot com will have to be enough for now
-				{icon: 'blueSky', label: 'Bluesky', href: 'https://bsky.app/profile/vokapelman.bsky.social'},
-				{icon: 'substack', label: 'Substack', href: 'https://vokapelman.substack.com'},
-				{icon: 'linkedin', label: 'LinkedIn', href: 'https://www.linkedin.com/in/victorkane' },
-				*/
-			],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+        },
+        social: [
+            {icon: 'github', label: 'GitHub', href: 'https://github.com/praxispoetica/dtad-witpl-2026'},
+            /* no space, so linktree style dot com will have to be enough for now
+            {icon: 'blueSky', label: 'Bluesky', href: 'https://bsky.app/profile/vokapelman.bsky.social'},
+            {icon: 'substack', label: 'Substack', href: 'https://vokapelman.substack.com'},
+            {icon: 'linkedin', label: 'LinkedIn', href: 'https://www.linkedin.com/in/victorkane' },
+            */
+        ],
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                items: [{ autogenerate: { directory: 'reference' } }],
+            },
+        ],
+		}), icon()],
 });
